790560e729a5a9384bbe6b376af01496c935e25e
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-08-06 21:33:28 -0700

    thursday update

--- script.js
+++ script.js
@@ -1,84 +1,82 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-
-function writePassword() { 
-  var passwordText = document.querySelector("#password");
-  var password = generatePW();
-
-  passwordText.value = password;
-}
-  
+var rndNumber = "0123456789";
+var rndSpecial = "!@#$%^&*()_+";
+var rndUpper = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+var rndLower = rndUpper.toLowerCase();
 
 document.getElementById("generate").onclick = function () {
   clickGp();
 };
 
-function generatePW() {
-  var rndNumber = "0123456789"
-  var rndSpecial = "!@#$%^&*()_+"
-  var rndUpper = "ABCDEFGHIJKLMNPQRSTUVWXYZ"
-  var rndLower = rndUpper.toLowerCase();
+function writePassword() {
+  var password = generatePW();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
-  console.log(getRandomValue(generateBtn));
+function generatePW() {
   console.log(getRandomValue(rndUpper));
   console.log(getRandomValue(rndLower));
   console.log(getRandomValue(rndSpecial));
-
+  console.log(getRandomValue(rndNumber));
 }
 
 function clickGp() {
-
+  
   var options = {};
 
-  options.generateBtn = parseInt( 
-    prompt("How many characters would you like between 8 and 128?") );
-    
-    if(options.generateBtn < 8 || options.generateBtn > 128 || isNaN(options.length)) {
-      alert("Incorrect length")
-      return("Invalid parameters")
+  options.length = parseInt(
+    prompt("How many characters would you like between 8 and 128?")
+  );
+
+  if (options.length < 8 || options.length > 128) {
+    alert("Incorrect length");
+    return;
+  }
+
+  options.rndNumber = confirm(
+    "Would you like to use numbers in your password?"
+  );
+
+  options.rndUpper = confirm(
+    "Would you like uppercase letters in your password?"
+  );
+
+  options.rndLower = confirm(
+    "Would you like lower case letters in your password?"
+  );
+
+  options.rndSpecial = confirm(
+    "would you like to use special characters in your password?"
+  );
+
+  var password = "";
+  for (var i = 0; i < options.length; i++) {
+    if (options.rndNumber) {
+      password += getRandomValue(rndNumber[i]);
     }
-    
-    options.rndNumber = confirm("Would you like to use numbers in your password?");
-    
-    
-    options.rndUpper = confirm("Would you like uppercase letters in your password?");
-    
-    options.rndLower = confirm("Would you like lower case letters in your password?");
-    
-    options.rndSpecial = confirm(
-      "would you like to use special characters in your password?"
-      );
-
-      var password = "";
-
-    for(var i = 0; i < options.generateBtn; i++) {
-
-      if (options.rndNumber) {
-        password += getRandomValue(rndNumber)
-        
-      }
-      if (options.rndUpper) {
-        password += getRandomValue(rndUpper)
-
-      }
-      if (options.rndLower) {
-        password += getRandomValue(rndLower)
-
-      }
-      if (options.rndSpecial) { 
-          password += getRandomValue(rndSpecial)
-      }
+    if (options.rndUpper) {
+      password += getRandomValue(rndUpper[i]);
     }
-    return password.substring(0, options.generateBtn);
-
+    if (options.rndLower) {
+      password += getRandomValue(rndLower[i]);
+    }
+    if (options.rndSpecial) {
+      password += getRandomValue(rndSpecial[i]);
+    }
+  }
+  console.log(options.length);
+  return password.substring(0, options.length);
 }
 
 function getRandomValue(str) {
   return str[Math.floor(Math.random() * str.length)];
 }
 
-function shuffle(str) {} 
-
+function shuffle(str) {}
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 //function writePassword() {
@@ -90,4 +88,3 @@ function shuffle(str) {}
 //}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
